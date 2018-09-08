31f965f13b85382f7bbff1ade1adcaba13cd7fdb
Daniel Knecht <daniel.knecht@t-online.de>
2018-09-08 14:46:39 +0200

    Beschreibung Entscheidungsbaum

--- assets/listings/quicksort.py
+++
@@ -1,8 +0,0 @@
-def quicksort(liste):
-if len(liste) <= 1:
-    return liste
-pivotelement = liste.pop()
-links = [element for element in liste if element < pivotelement]
-rechts = [element for element in liste if element >= pivotelement]
-return quicksort(links) + [pivotelement] + quicksort(rechts)
-# Quelle: http://de.wikipedia.org/wiki/Python_(Programmiersprache)
