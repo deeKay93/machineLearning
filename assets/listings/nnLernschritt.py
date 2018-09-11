06cce2c734f38f3bcd5d5fde075ef20698c6327a
poletin <lea@poletin.de>
2018-09-11 09:29:53 +0200

    Zusammenfassung / Fazit NN

--- assets/listings/nnLernschritt.py
+++ assets/listings/nnLernschritt.py
@@ -1,1 +1,2 @@
+cost_function = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y, labels=y_))
 training_step =tf.train.GradientDescentOptimizer(learning_rate).minimize(cost_function, global_step)
